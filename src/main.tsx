0553d869b61037db93bea316a1aacfec544d1138
Kenzy Soror <kenzy.soror.05@gmail.com>
2025-12-29 06:55:22 -0500

    Remove viewport scaler

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './css/viewport-scale.css'
 import './css/audio.css'
 import App from './App.tsx'
-import ViewportScaler from './components/ViewportScaler'
 import GlobalAudio from './components/GlobalAudio'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ViewportScaler>
-      <GlobalAudio />
-      <App />
-    </ViewportScaler>
+    <GlobalAudio />
+    <App />
   </StrictMode>,
 )
